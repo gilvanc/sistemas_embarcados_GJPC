b17f80ffc2b099136e555adc7f97d03d02bff5c0
gilvanc <gilvan.jpc@gmail.com>
2018-03-21 22:46:55 -0300

    atualizando atividades

--- questoes/aula 03/atividades/ola_num_caracs_1.c
+++ questoes/aula 03/atividades/ola_num_caracs_1.c
@@ -5,9 +5,9 @@
 int main(int argc, char **argv)
 {
 	int i=0;
-	for(i=0;i<=argc;i++)
+	for(i=0;i<argc;i++)
 	{
-		printf("Argumento: %s / Numero de caracteres: %d", argv[i], Num_Caracs(argv[i]));
+		printf("Argumento: %s / Numero de caracteres: %d\n", argv[i], Num_Caracs(argv[i]));
 	}	
 	return 0;
 }
